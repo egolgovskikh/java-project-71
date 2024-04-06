42d4e3f0e18089f189aa94db8b0832f5a1b177e4
egolgovskikh <egolgovskikh@gmail.com>
2024-04-06 11:10:42 +0300

    task #10

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -29,23 +29,6 @@ class ParserTest {
     }
 
     @Test
-    void testReadYmlFile() {
-        Map<String, Object> actual;
-        try {
-            actual = Parser.readFile("src/test/resources/yml/testFile3.yml");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Map<String, Object> expected = new HashMap<>();
-        expected.put("host", "hexlet.io");
-        expected.put("timeout", 50);
-        expected.put("proxy", "123.234.53.22");
-        expected.put("follow", false);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
     void testReadUnsupportedExtensionFile() {
         assertThrows(IOException.class, () -> Parser.readFile("src/test/resources/txt/testFile.txt"));
     }
